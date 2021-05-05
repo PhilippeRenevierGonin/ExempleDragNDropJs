76688c1227040048b605af03f57086f7ccb589b2
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2021-05-05 16:09:12 +0200

    Ajout d'un drop possibler dans le plateau (avec un cadre en tiret pour le visualiser). Ceci nécessite  event.stopPropagation(); pour éviter que l'événement soit pris en compte et par une carte et par le plateau.

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -1,3 +1,36 @@
+class Plateau {
+    constructor(eltHtml) {
+        this.plateauHtml = eltHtml;
+
+        this.plateauHtml.addEventListener("dragover", ( event ) => {
+            // prevent default to allow drop
+            event.preventDefault();
+            console.log("dragover sur plateau")
+        });
+
+        this.plateauHtml.addEventListener("drop", (e) => this.dropUneCarte(e));
+
+    }
+
+    dropUneCarte(event) {
+        let carteDéplacée = document.getElementById(event.dataTransfer.getData("text/id"));
+        carteDéplacée.ctrl.memoriserAnciennePosition();
+
+        let positionDuPlateau = this.plateauHtml.getBoundingClientRect();
+
+        let xInit = parseInt(event.dataTransfer.getData("text/posX"));
+        let yInit = parseInt(event.dataTransfer.getData("text/posY"));
+        let x = event.clientX - positionDuPlateau.x - xInit;
+        let y = event.clientY - positionDuPlateau.y - yInit;
+
+        carteDéplacée.style.left=""+x+"px";
+        carteDéplacée.style.top=""+y+"px";
+
+    }
+
+}
+
+
 class Carte {
 
     static ESPACEMENT = 20; //px
@@ -15,9 +48,10 @@ class Carte {
        this.carteHtml.addEventListener("dragover", ( event ) => {
            // prevent default to allow drop
            event.preventDefault();
+           event.stopPropagation();
            console.log("drag over sur "+this.carteHtml.id);
        });
-       this.carteHtml.addEventListener("drop", (e) => this.dropUneAutreCarteCarte(e));
+       this.carteHtml.addEventListener("drop", (e) => this.dropUneAutreCarte(e));
 
                // creation de la minature
                let span = document.createElement("span");
@@ -42,10 +76,22 @@ class Carte {
     }
 
     drag(e) {
+        e.stopPropagation();
+
         e.dataTransfer.setData("text/id", this.carteHtml.id);
+        
+        // position de la souris sur la carte
+        // calcul car rien de standard
+        let positionDeLaCarte = this.carteHtml.getBoundingClientRect();
+        let x = e.clientX - positionDeLaCarte.x; // layerX n'est pas standard, on calcul
+        let y = e.clientY - positionDeLaCarte.y;
+        e.dataTransfer.setData("text/posX", x);
+        e.dataTransfer.setData("text/posY", y);
+
     }
 
-    dropUneAutreCarteCarte(e) {
+    dropUneAutreCarte(e) {
+        e.stopPropagation();
         console.log("drop sur "+this.carteHtml.id+" de "+e.dataTransfer.getData("text/id"));
         let carteDéplacée = document.getElementById(e.dataTransfer.getData("text/id"));
         // console.log("drop possible ? "+this.peutRecevoir(carteDéplacée.ctrl));
